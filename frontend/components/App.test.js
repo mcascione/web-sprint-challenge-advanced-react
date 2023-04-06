de6a468dd81714aeaff512aa9903e30970d3a465
Miranda Cascione <miranda.cascione@gmail.com>
2023-04-05 19:49:19 -0700

    added additional react testing

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,5 +1,44 @@
 // Write your tests here
+import AppFunctional from "./AppFunctional";
+import React from "react";
+import '@testing-library/jest-dom';
+import { render, screen, fireEvent } from '@testing-library/react';
+
 test('sanity', () => {
   expect(true).toBe(true)
 })
 
+test('steps header is visible', () => {
+  render (<AppFunctional />);
+  const stepsHeader = screen.getByRole('heading', {name: (/you moved/i)});
+  expect(stepsHeader).toBeTruthy();
+  expect(stepsHeader).toBeVisible();
+});
+
+test('counter header is visible', () => {
+  render (<AppFunctional />);
+  const coordinatesHeader = screen.getByRole('heading', {name: (/coordinates/i)});
+  expect(coordinatesHeader).toBeTruthy();
+  expect(coordinatesHeader).toBeVisible();
+});
+
+test('counter increments when up, down, left or right buttons are clicked', () => {
+  render (<AppFunctional />);
+  const stepsHeader = screen.getByRole('heading', {name: (/you moved/i)});
+  const upButton = screen.getByRole('button', {name:/up/i});
+  const downButton = screen.getByRole('button', {name:/down/i});
+  const leftButton = screen.getByRole('button', {name:/left/i});
+  const rightButton = screen.getByRole('button', {name:/right/i});
+  fireEvent.click(upButton, downButton, leftButton, rightButton);
+  expect(stepsHeader === "You moved 4 times"); 
+})
+
+test('display a message when a valid email is submitted', async () => {
+  render (<AppFunctional />);
+  const emailInput = screen.getByPlaceholderText('type email');
+  fireEvent.change(emailInput, {target:{value: "miranda@gmail.com"}});
+  const submitButton = screen.getByTestId("submit-button");
+  fireEvent.click(submitButton);
+  const message = await screen.findByTestId('message');
+  expect(message).toBeVisible();
+});
--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -104,7 +104,7 @@ export default function AppFunctional(props) {
         ))}
       </div>
       <div className="info">
-        <h3 id="message">{message}</h3>
+        <h3 id="message" data-testid="message">{message}</h3>
       </div>
       <div id="keypad">
         <button id="left" onClick={move} count={count}>LEFT</button>
@@ -114,7 +114,7 @@ export default function AppFunctional(props) {
         <button id="reset" onClick={reset} >reset</button>
       </div>
       <form>
-        <input id="email" type="email" placeholder="type email" onChange={onChange} value={email}></input>
+        <input id="email" type="email" placeholder="type email" onChange={onChange} value={email} data-testid="submit-button"></input>
         <input id="submit" type="submit" onClick={onSubmit}></input>
       </form>
     </div>
